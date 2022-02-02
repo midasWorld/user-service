ab9f16a36c52a5e0ceaf9df5649e560d8b66117b
midasworld <midas117@knou.ac.kr>
2022-02-02 23:09:23 +0900

    인사관리 프로젝트 생성

---
+++ src/test/java/com/midas/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
