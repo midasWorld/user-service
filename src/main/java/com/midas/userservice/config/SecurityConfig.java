160a59c8d043f160c201884f4a1ea1206f83c200
midasworld <midas117@knou.ac.kr>
2022-02-05 19:15:17 +0900

    feat : Spring Security 추가

---
+++ src/main/java/com/midas/userservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.midas.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .formLogin().disable()
+        ;
+    }
+}
