20bff9bf7dce3f9ea1f5c61fda2f0efff6a46546
midasworld <midas117@knou.ac.kr>
2022-02-08 16:26:31 +0900

    feat: jwt 라이브러리, 관련 정보 추가 & 정보 암호화 By Jasypt

--- src/test/java/com/midas/userservice/config/JasyptConfigTest.java
+++ src/test/java/com/midas/userservice/config/JasyptConfigTest.java
@@ -14,19 +14,36 @@ class JasyptConfigTest {
         String url = "url"; // url - ⭐️
         String username = "username"; // username - ⭐️
         String password = "password"; // password - ⭐
+        
+        String jwtSecret = "jwtSecret";
+        String jwtExpiresSec = "jwtExpiresSec";
+        String jwtSubject = "jwtSubject";
+        String bcryptSaltRounds = "bcryptSaltRounds";
 
         setup(key);
         String encryptUrl = jasyptEncrypt(url);
         String encryptUsername = jasyptEncrypt(username);
         String encryptPassword = jasyptEncrypt(password);
+        String encryptJwtSecret = jasyptEncrypt(jwtSecret);
+        String encryptJwtExpiresSec = jasyptEncrypt(jwtExpiresSec);
+        String encryptJwtSubject = jasyptEncrypt(jwtSubject);
+        String encryptBcryptSaltRounds = jasyptEncrypt(bcryptSaltRounds);
 
         System.out.println("encryptUrl: " + encryptUrl);
         System.out.println("encryptUsername: " + encryptUsername);
         System.out.println("encryptPassword: " + encryptPassword);
+        System.out.println("encryptJwtSecret: " + encryptJwtSecret);
+        System.out.println("encryptJwtExpiresSec: " + encryptJwtExpiresSec);
+        System.out.println("encryptJwtSubject: " + encryptJwtSubject);
+        System.out.println("encryptBcryptSaltRounds: " + encryptBcryptSaltRounds);
 
         Assertions.assertThat(jasyptDecrypt(encryptUrl)).isEqualTo(url);
         Assertions.assertThat(jasyptDecrypt(encryptUsername)).isEqualTo(username);
         Assertions.assertThat(jasyptDecrypt(encryptPassword)).isEqualTo(password);
+        Assertions.assertThat(jasyptDecrypt(encryptJwtSecret)).isEqualTo(jwtSecret);
+        Assertions.assertThat(jasyptDecrypt(encryptJwtExpiresSec)).isEqualTo(jwtExpiresSec);
+        Assertions.assertThat(jasyptDecrypt(encryptJwtSubject)).isEqualTo(jwtSubject);
+        Assertions.assertThat(jasyptDecrypt(encryptBcryptSaltRounds)).isEqualTo(bcryptSaltRounds);
     }
 
     private void setup(String key) {
