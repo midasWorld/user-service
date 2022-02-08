17401b4901cb5b76715fc7003be71ff98be0dee3
midasworld <midas117@knou.ac.kr>
2022-02-08 15:38:17 +0900

    refactor: [Auth 개발 전] Test, data.sql 삭제

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into users(email, name, password) values ('user1@gmail.com', 'user1', '1234');
-insert into users(email, name, password) values ('user2@gmail.com', 'user2', '1234');
-insert into users(email, name, password) values ('user3@gmail.com', 'user3', '1234');
-insert into users(email, name, password) values ('user4@gmail.com', 'user4', '1234');
