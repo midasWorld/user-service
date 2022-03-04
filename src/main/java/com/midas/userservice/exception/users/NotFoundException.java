ac36bbe7bf62f727151607d6a0acdf5e9403bc5f
midasworld <midas117@knou.ac.kr>
2022-03-05 00:38:02 +0900

    refactor: UserNotFoundException, RoleNotFoundException → NotFoundException 으로 통합

--- src/main/java/com/midas/userservice/exception/users/NotFoundException.java
+++ src/main/java/com/midas/userservice/exception/users/NotFoundException.java
@@ -4,9 +4,13 @@ import com.midas.userservice.exception.ServiceRuntimeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// HTTP Status code
+// 2XX → OK
+// 3XX → Client
+// 5XX → Server
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class RoleNotFoundException extends ServiceRuntimeException {
-    public RoleNotFoundException(String message) {
+public class NotFoundException extends ServiceRuntimeException {
+    public NotFoundException(String message) {
         super(message);
     }
 }
